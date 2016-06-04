589fb869481091ae9ab41de964a84e3205f3699d
Mark D <mark@qstrike.com>
2016-06-04 15:35:36 +0800

    Status:Feat;Theme Underscore and optionframe
    * Markd First Commit`

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Mark_Frame_Theme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'custom_framework' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'custom_framework' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'custom_framework' ), 'custom_framework', '<a href="http://www.markd.co.nf" rel="designer">Mark Erenz Duque</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Mark Frame Theme Theme Customizer.
+ *
+ * @package Mark_Frame_Theme
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function custom_framework_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'custom_framework_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function custom_framework_customize_preview_js() {
+	wp_enqueue_script( 'custom_framework_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'custom_framework_customize_preview_js' );
